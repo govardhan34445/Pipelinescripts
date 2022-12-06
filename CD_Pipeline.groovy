bc6f54b8ae6518a86d81c3b12f5a432ce3fead7c
govardhan34445 <60291012+govardhan34445@users.noreply.github.com>
2022-12-06 15:52:38 +0530

    Add files via upload

---
+++ CD_Pipeline.groovy
@@ -0,0 +1,83 @@
+pipeline{
+
+  agent any
+
+  properties([
+    parameters([
+        
+        string(
+            defaultValue: 'dev', 
+            name: 'namespace'
+        ),
+        string(
+            defaultValue: 'dev_cluster', 
+            name: 'cluster_name'
+        ),
+        string(
+            defaultValue: 'us-east-2', 
+            name: 'region'
+        )
+    ])
+  ])
+
+  stages{
+
+      stage('CheckOutCode'){
+        steps{
+        git branch: 'development', credentialsId: 'github_cred_id', url: 'github_repo_url'
+    
+        }
+      }
+  
+      stage('Login to EKS Cluster'){
+        steps{
+            withCredentials([[ $class: 'AmazonWebServicesCredentialsBinding', credentialsId: "aws-credentials-id-here", accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                sh  """
+                export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" && export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+                aws eks --region ${region} update-kubeconfig --name ${cluster_name}        
+                """
+            }
+        }
+      }
+
+      stage('Deploy Application into EKS cluster'){
+        steps{
+          sh  """
+            kubectl apply -f deployment_file.yaml
+            kubectl apply -f service_file.yaml
+            """
+        }
+      }
+      
+      stage('Deployment Post Checks'){
+        steps{
+          sh  """
+             kubectl get pods -n ${namespace}
+             Kubectl get deployment -n ${namespace}
+             kubectl get svc -n ${namespace}
+              """
+        }
+      }
+      
+  }//Stages Closing
+
+post{
+
+ success{
+ emailext to: 'govardhan34445@gmail.com',
+          subject: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          body: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          replyTo: 'govardhan34445@gmail.com'
+ }
+ 
+ failure{
+ emailext to: 'govardhan34445@gmail.com,34445govardhan@gmail.com',
+          subject: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          body: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          replyTo: 'govardhan34445@gmail.com'
+ }
+ 
+}
+
+
+}//Pipeline closing
---
+++ CI_Pipeline.groovy
@@ -0,0 +1,63 @@
+pipeline{
+
+  agent any
+
+  stages{
+
+      stage('CheckOutCode'){
+        steps{
+        git branch: 'development', credentialsId: 'github_cred_id', url: 'github_repo_url'
+    
+        }
+      }
+  
+      stage('Build'){
+        steps{
+          sh  "mvn clean package"
+        }
+      }
+
+      stage('ExecuteSonarQubeReport'){
+        steps{
+          sh  "mvn clean sonar:sonar"
+        }
+      }
+      
+      stage('Build Docker Image'){
+        steps{
+          sh  " docker build -t imagename:tag"
+        }
+      }
+      
+      stage('Push Docker Image to Artifactory'){
+        steps{
+                  withCredentials([usernamePassword(credentialsId: 'myregistry-login', passwordVariable: 'DOCKER_REGISTRY_PWD', usernameVariable: 'DOCKER_REGISTRY_USER')]) {
+                                  sh "docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PWD} artifactory_Url"
+                                  sh "docker push imagename:tag"
+                                  sh "docker rmi imagename:tag "
+
+        }
+        }
+      }
+  }//Stages Closing
+
+post{
+
+ success{
+ emailext to: 'govardhan34445@gmail.com',
+          subject: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          body: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          replyTo: 'govardhan34445@gmail.com'
+ }
+ 
+ failure{
+ emailext to: 'govardhan34445@gmail.com,34445govardhan@gmail.com',
+          subject: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          body: "Pipeline Build is over .. Build # is ..${env.BUILD_NUMBER} and Build status is.. ${currentBuild.result}.",
+          replyTo: 'govardhan34445@gmail.com'
+ }
+ 
+}
+
+
+}//Pipeline closing
