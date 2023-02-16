f0576d850edd772e04d4f9551fcfb6978c450d1e
govardhan34445 <60291012+govardhan34445@users.noreply.github.com>
2023-02-16 11:58:59 +0530

    Create junit.groovy

---
+++ junit.groovy
@@ -0,0 +1,16 @@
+pipeline {
+   agent any
+   stages {
+     stage('Build and Test') {
+        steps {
+            sh 'build here...'
+            sh 'run tests here if you like ...'
+        }
+     }
+   }
+   post {
+      always {
+        junit '**/reports/junit/*.xml'
+      }
+   } 
+}
